f5cc3b5f3bb4ddcd2a158d9c2ce90019cb4f4a64
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-19 22:50:32 +0530

    Add expo-linear-gradient and implement gradient background on index screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <LinearGradient colors={["#151518", "#2B3037"]} style={styles.background}>
+        <Text style={styles.text}>Gradient Background</Text>
+      </LinearGradient>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: { color: "white", fontSize: 20 },
+});
